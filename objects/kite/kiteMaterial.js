0368a652754eea6afb5d5bbb4f73667545300475
Samuel Schnelly <schnelly@scgp.sk>
2023-12-20 09:49:55 +0100

    update aby sa to viac podobalo na sarkana...

--- objects/kite/kiteMaterial.js
+++ objects/kite/kiteMaterial.js
@@ -2,4 +2,11 @@ import * as THREE from "three";
 
 const KITE_MATERIAL = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.DoubleSide, transparent: true})
 
-export default KITE_MATERIAL
+// load from image
+const KITE_MATERIAL2 = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load('assets/kitie/kittie3.png'),
+    side: THREE.DoubleSide,
+    transparent: true
+});
+
+export default KITE_MATERIAL2
